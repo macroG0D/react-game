3f4729590bdf8feff572e1cbfcc301540b39d86f
TonYem <tonyrhesus@gmail.com>
2021-02-25 23:34:08 +0200

    feat: add canvas and its auto resize

--- src/components/game/game.js
+++ src/components/game/game.js
@@ -1,15 +1,45 @@
 import React, { Component } from 'react';
+import { SAFEZONE, MARGIN } from '../../static/constans';
 
 import './game.scss';
 
 export default class Game extends Component {
+  state = {
+    width: 984,
+    height: 600,
+  };
+
+  calculateCanvasSize = (width) => {
+    return Math.floor(width / 1.777);
+  };
+
+  checkWindowSize = () => {
+    let newWidth = window.innerWidth - MARGIN;
+    let newHeight = this.calculateCanvasSize(newWidth);
+    while (newHeight > window.innerHeight - SAFEZONE) {
+      newWidth -= 10;
+      newHeight = this.calculateCanvasSize(newWidth);
+    }
+    this.setState({
+      width: newWidth,
+      height: newHeight,
+    });
+  };
+
+  componentDidMount() {
+    this.checkWindowSize();
+    window.addEventListener('resize', () => this.checkWindowSize());
+  }
+
   render() {
-    const game = 'Game';
+    const { width, height } = this.state;
     return (
       <div className="game">
-        <div className="center-content">
-          <h1>{game}</h1>
-        </div>
+        <canvas
+          id="canvas"
+          width={`${width}px`}
+          height={`${height}px`}
+        ></canvas>
       </div>
     );
   }
---
+++ src/static/constans.js
@@ -0,0 +1,4 @@
+const SAFEZONE = 200;
+const MARGIN = 40;
+
+export {SAFEZONE, MARGIN};
