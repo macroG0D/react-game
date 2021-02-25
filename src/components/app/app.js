480fd477b83472b6c9655cc40ca4e348ec967696
TonYem <tonyrhesus@gmail.com>
2021-02-25 14:08:31 +0200

    init: react

---
+++ src/components/app/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './app.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1> React game init </h1>
+    </div>
+  );
+}
+
+export default App;
